557ee36fe1087037b72a070834cf6bd6576e1c8e
lucidiusss <cheapp78@gmail.com>
2024-06-07 16:39:46 +0300

    upd

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,26 +6,45 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import { Skeleton } from "@/components/ui/skeleton";
 
 function Cards({ items }) {
   return (
     <div className="flex flex-row gap-16 mx-40 my-5">
       <Carousel className="w-full">
-        {items.length > 4 && (
-          <div>
-            <CarouselNext />
-            <CarouselPrevious />
-          </div>
+        <CarouselNext />
+        <CarouselPrevious />
+        {items.length > 0 && (
+          <CarouselContent>
+            {items &&
+              items.map((item, index) => (
+                <CarouselItem className="p-10 basis-4/4" key={index}>
+                  <CardItem item={item} />
+                </CarouselItem>
+              ))}
+          </CarouselContent>
         )}
-        <CarouselContent>
-          {items &&
-            items.map((item, index) => (
-              <CarouselItem key={index} className="p-5 basis-4/4">
-                <CardItem item={item} />
-              </CarouselItem>
-            ))}
-        </CarouselContent>
+        {items.length === 0 && (
+          <CarouselContent>
+            <CarouselItem className="p-10 basis-4/4">
+              <Skeleton className="min-w-[300px] w-[300px] h-[500px]  border-2 rounded-xl flex flex-col border-[#2f2f2f]" />
+            </CarouselItem>
+            <CarouselItem className="p-10 basis-4/4">
+              <Skeleton className="min-w-[300px] w-[300px] h-[500px]  border-2 rounded-xl flex flex-col border-[#2f2f2f]" />
+            </CarouselItem>
+            <CarouselItem className="p-10 basis-4/4">
+              <Skeleton className="min-w-[300px] w-[300px] h-[500px]  border-2 rounded-xl flex flex-col border-[#2f2f2f]" />
+            </CarouselItem>
+            <CarouselItem className="p-10 basis-4/4">
+              <Skeleton className="min-w-[300px] w-[300px] h-[500px]  border-2 rounded-xl flex flex-col border-[#2f2f2f]" />
+            </CarouselItem>
+            <CarouselItem className="p-10 basis-4/4">
+              <Skeleton className="min-w-[300px] w-[300px] h-[500px]  border-2 rounded-xl flex flex-col border-[#2f2f2f]" />
+            </CarouselItem>
+          </CarouselContent>
+        )}
       </Carousel>
+      ;
     </div>
   );
 }
